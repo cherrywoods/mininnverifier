f8f610466e0f69c861d5d827013d011f3fc3c5a3
David Boetius <david.boetius@uni-konstanz.de>
2026-04-15 16:41:59 +0200

    Serialize compute graphs

--- src/mininnverifier/minijax/serialize.py
+++ src/mininnverifier/minijax/serialize.py
@@ -15,7 +15,10 @@ Variables are lower case, constants are upper case.
 from pathlib import Path
 import zipfile
 
-from .compute_graph import ComputeGraph, Const, Var
+import numpy as np
+
+from . import core as minijax_core
+from .compute_graph import ComputeGraph, Const, Equation, Var
 
 
 def dump(graph: ComputeGraph, file: str | Path):
@@ -25,7 +28,8 @@ def dump(graph: ComputeGraph, file: str | Path):
         with zf.open("graph.txt", "w") as f:
             f.write(graph_str.encode("utf-8"))
 
-        const_bytes = {name: const.value.tobytes("c") for name, const in consts.items()}
+        consts = {name: const.value.astype(np.float64) for name, const in consts.items()}
+        const_bytes = {name: val.tobytes("c") for name, val in consts.items()}
         for name, bytes in const_bytes.items():
             with zf.open(f"{name}.bin", "w") as f:
                 f.write(bytes)
@@ -60,7 +64,7 @@ def _serialize_graph(graph) -> tuple[str, dict[str, Const]]:
         return name + "[" + ", ".join(map(str, atom.shape)) + "]"
 
     def serialize_eqn(eqn):
-        opts = "{" + ", ".join([f"{k}={v}" for k, v in eqn.options.items()]) + "}"
+        opts = "{" + ", ".join([f"{k}: {v}" for k, v in eqn.options.items()]) + "}"
         repr = f"{serialize_atom(eqn.outvar)} = {eqn.primitive.name}{opts} "
         return repr + " ".join(map(serialize_atom, eqn.inputs))
 
@@ -73,6 +77,46 @@ def _serialize_graph(graph) -> tuple[str, dict[str, Const]]:
 
 
 def _deserialize_graph(graph_repr: str, consts: dict[str, bytes]) -> ComputeGraph:
-    vars = {}
+    lines = graph_repr.splitlines()
+    input_line = lines[0].strip()
+    output_line = lines[-1].strip()
+    atoms = {}
 
-    # TODO: implement
+    def deserialize_atom(repr: str):
+        name, shape_str = repr.split("[")
+        name = name.strip()
+        shape = shape_str[:-1].split(",")
+        shape = tuple(int(s.strip()) for s in shape)
+
+        if name not in atoms:
+            if name.upper() == name:  # uppercase => Const
+                val_bytes = consts[name]
+                val = np.frombuffer(val_bytes).reshape(shape)
+                atoms[name] = Const(val)
+            else:
+                atoms[name] = Var(shape)
+
+        atom = atoms[name]
+        assert atom.shape == shape, f"Inconsistent shapes for {name}: {atom.shape}, {shape}."
+        return atom
+
+    def deserialize_eqn(repr: str):
+        outvar, expr = repr.split("=")
+        primitive, *inputs = expr.strip().split()
+        primitive, opts = primitive.split("{")
+
+        primitive = getattr(minijax_core, primitive)
+        opts = opts[:-1].strip().split(",")
+        opts = [opt.split(":") for opt in opts]
+        opts = {key.strip(): val.strip() for key, val in opts}
+
+        _, outvar = deserialize_atom(outvar)
+        inputs = tuple(deserialize_atom(iv)[1] for iv in inputs)
+        return Equation(primitive, inputs, outvar, opts)
+
+    invars = input_line[len("input:") :].split(",")
+    invars = tuple(map(deserialize_atom, invars))
+    eqns = tuple(map(deserialize_eqn, lines))
+    outvars = output_line[len("output:") :].split(",")
+    outvars = tuple(map(deserialize_atom, outvars))
+    return ComputeGraph(invars, outvars, eqns)
